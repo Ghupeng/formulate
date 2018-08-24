b611dc238b545bd7653827d1990d2f233012f80c
huguopeng <gp.hu@yinchenglicai.com>
2018-08-24 10:34:12 +0800

    添加文件

---
+++ src/Demo.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: huguopeng
+ * Date: 2018/08/24
+ * Time: 10:27
+ */
+namespace framing;
+class Demo{
+    public function test(){
+        echo "This is framint __function__ test.";
+    }
+}
