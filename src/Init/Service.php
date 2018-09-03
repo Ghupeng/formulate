4f3099644e0e62eae3d6cd3c686ea9651628f96e
Ghupeng <ghupeng@163.com>
2018-09-03 20:53:36 +0800

    fix notes

--- src/Init/Service.php
+++ src/Init/Service.php
@@ -9,16 +9,12 @@
 namespace framing\Init;
 use Phalcon\Mvc\View;
 use Phalcon\Mvc\View\Engine\Php as PhpEngine;
-//use Phalcon\Mvc\Url as UrlResolver;
 use Phalcon\Mvc\View\Engine\Volt as VoltEngine;
 use Phalcon\Mvc\Model\Metadata\Memory as MetaDataAdapter;
 use Phalcon\Session\Adapter\Files as SessionAdapter;
-//use Phalcon\Flash\Direct as Flash;
 
 class Service {
-	/**
-	 * @param $di
-	 */
+
 	public static function init(&$di) {
 		/**
 		 * Setting up the view component
