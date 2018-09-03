6de64fb56e8e7bca3a26a2f3c42467bac3a20e6a
Ghupeng <ghupeng@163.com>
2018-09-03 20:23:40 +0800

    add autoload

--- src/Init/Loader.php
+++ src/Init/Loader.php
@@ -6,5 +6,22 @@
  * Time: 16:04:12
  * By: Loader.php
  */
+namespace framing\Init;
 
+class Loader {
+	
+	public static function init(){
+		$loader = new \Phalcon\Loader();
+		$config = \framing\Library\ConfigLibrary::get('config','application');
+		/**
+		 * We're a registering a set of directories taken from the configuration file
+		 */
+		$loader->registerDirs(
+		   [
+			$config->controllersDir,
+			$config->modelsDir		
+		   ]
+		)->register();
+	}
+}
 
