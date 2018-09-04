53fe3d75cc4f6f34f011a9aae06bcd16a37c8c65
Ghupeng <ghupeng@163.com>
2018-09-04 19:19:27 +0800

    add router execute

---
+++ src/Plugin/RouterExecutePlugin.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by vim.
+ * User: huguopeng
+ * Date: 2018/09/04
+ * Time: 19:07:38
+ * By: RouterExecutePlugin.php
+ */
+namespace framing\Plugin;
+use Exception;
+use Phalcon\Events\Event;
+use Phalcon\Mvc\Dispatcher;
+use Phalcon\Mvc\Dispatcher\Exception as DispatchException;
+
+class RouterExecutePlugin {
+
+	public function beforeExecuteRoute(Event $event, Dispatcher $dispatcher, Exception $exception)
+	{
+		// Default error action
+		$action = 'show503';
+
+		// Handle 404 exceptions
+		if ($exception instanceof DispatchException) {
+			$action = 'show404';
+		}
+		header("Location: https://www.baidu.com/");
+		return false;
+	}
+}
