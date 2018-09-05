403d1504782b653251050cf13bf2f87347ddcf71
Ghupeng <ghupeng@163.com>
2018-09-05 14:04:54 +0800

    add baseController

---
+++ src/Base/BaseController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by vim.
+ * User: huguopeng
+ * Date: 2018/09/05
+ * Time: 14:02:52
+ * By: BaseController.php
+ */
+namespace framing\Base;
+
+class BaseController extends ControllerBase {
+	
+	public function execute(){
+		
+	}
+}
---
+++ src/Base/BaseException.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by vim.
+ * User: huguopeng
+ * Date: 2018/09/04
+ * Time: 10:10:29
+ * By: BaseException.php
+ */
+ namespace framing\Base;
+
+class BaseException extends \Exception {
+    const SUCCESS           = 1000;
+    const FROM_CHECK_FAIL   = 2000;
+    const BAN_LOGIN         = 3000;
+    const DATA_IS_EMPTY     = 4000;
+    const INTER_ERROR       = 5000;
+
+    public static $messages = [
+        self::SUCCESS           => '成功',
+        self::FROM_CHECK_FAIL   => '参数错误',
+        self::BAN_LOGIN         => '账号被锁定',
+        self::DATA_IS_EMPTY     => '未找到数据',
+        self::INTER_ERROR       => '服务器开小差',
+    ];
+    public function __construct($message = "", $code = 0) {
+        $this->code     = $code;
+        $this->message  = self::getErrorMessage($code);
+    }
+    public static function getErrorMessage($code = '') {
+        if(isset(self::$messages[$code])){
+            return self::$messages[$code];
+        }
+    }
+}
