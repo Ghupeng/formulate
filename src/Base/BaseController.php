c6e4d3325b65b0f839ec2863b4c261c4d4c23955
Ghupeng <ghupeng@163.com>
2018-09-07 17:28:51 +0800

    fix BaseController execute

--- src/Base/BaseController.php
+++ src/Base/BaseController.php
@@ -3,14 +3,22 @@
  * Created by vim.
  * User: huguopeng
  * Date: 2018/09/05
- * Time: 14:02:52
+ * Time: 20:02:52
  * By: BaseController.php
  */
 namespace framing\Base;
 
-class BaseController extends ControllerBase {
-	
-	public function execute(){
-		
+class BaseController extends \ControllerBase {
+
+	public function execute() {
+		$basePageInfo = (\Phalcon\DI::getDefault())->get("basePageInfo");
+		$class_name = "\\".APP_NAMESPACE."\\modules\\$basePageInfo->module\\Service\\".$basePageInfo->method;
+		if(class_exists($class_name)) {
+			$object = new $class_name();
+			$object->execute();
+		} else {
+			echo  "不存在";// 抛出异常
+		}
+
 	}
 }
